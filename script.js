f6508adf36637d341dfe7f6726f0ab68ba961d00
Moshe Ben Baruch <MosheBe@eladsystems.co.il>
2023-01-25 17:37:40 +0200

    part 2

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 //global variable
 const userName = 'Moshe';
+console.log(age); //undefined
 
 setTimeout(() => {
     //local variable
+    const age = 99;
+    console.log(age); //99
+
+
     const userName = 'Yaron';
     console.log(userName);
 },1000);
+
+console.log(age); //undefined
 
 
 
