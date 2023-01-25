8d163b4506ffcc04096f896fb0a7ec22efd98bdd
Moshe Ben Baruch <MosheBe@eladsystems.co.il>
2023-01-25 18:35:27 +0200

    class example 1

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-console.log('hello OOP');
-console.log('second commit');
+console.log('example object');
+
+const Person = {
+    firstName: 'Moshe',
+    lastName: 'Ben',
+    age: 99,
+    fullName: function(){
+        return `${this.firstName} ${this.lastName}`
+    },
+    setFirstName: function(newFirstName){
+      this.firstName = newFirstName;  
+    }
+}
+
+Person.setFirstName('Yaron');
+console.log(Person.fullName());
