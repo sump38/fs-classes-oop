b9dcd19ed5833f0eccf127f5ceb63c26013a86f8
Moshe Ben Baruch <MosheBe@eladsystems.co.il>
2023-02-01 18:22:35 +0200

    base code for getters and setters

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
-console.log('hello OOP');
-console.log('second commit');
+class Person {
+    
+    constructor(name = 'anonymous', lastName, age) {
+        this.name = name;
+        this.lastName = lastName ? lastName : '';
+        this._age = age > 0 ? age : 0;
+    }
+
+    get fullName() {
+        return this.name + ' ' + this.lastName;
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(value) {
+        if(value < 0) {
+            this._age = 0;
+        } else 
+            this._age = value;
+    }
+
+    sayName() {
+        console.log(`hello, my name is ${this.name}!`);
+    }
+}
+
+const myPerson = new Person('Moshe');
+const myOtherPerson = new Person();
+myPerson.sayName();
+myOtherPerson.sayName();
