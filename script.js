e55a819729dadd4af867d4bac26efaa30f28108f
Moshe Ben Baruch <MosheBe@eladsystems.co.il>
2023-02-01 19:28:10 +0200

    extend behaviour

--- script.js
+++ script.js
@@ -1,2 +1,41 @@
-console.log('hello OOP');
-console.log('second commit');
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    speak() {
+        console.log(`${this.name} says hello world`);
+    }
+
+    eat() {
+        console.log(`${this.name} eats!`);
+    }
+
+}
+
+class Dog extends Animal {
+    speak() {
+        console.log(`${this.name} says bark!`);
+    }
+
+    scratch() {
+        console.log(`${this.name} scratches`);
+    }
+}
+
+class Cat extends Dog {
+    speak() {
+        console.log(`${this.name} says meow!`);
+    }
+}
+
+
+
+const human = new Animal('Moshe');
+human.speak();
+
+const rex = new Dog('Rex');
+rex.speak();
+
+const kitty = new Cat('kitty');
+kitty.speak();
