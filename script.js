dafe7ca8fdf269496a3d4327ce1feebe3d336b1e
Moshe Ben Baruch <MosheBe@eladsystems.co.il>
2023-02-01 20:14:44 +0200

    base for school simulation

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
-console.log('hello OOP');
-console.log('second commit');
+class Human {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    speak() {
+        console.log('hello world');
+    }
+
+
+}
+
+class Student extends Human {
+
+    constructor(firstName, lastName, topics) {
+        super(firstName, lastName);
+        this.topics = topics;
+    }
+
+    speak() {
+        super.speak();
+        console.log(`I'm a student`);
+    }
+}
+
+class Teacher extends Human {
+    constructor(firstName, lastName, topic) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.topic = topic;
+    }
+} 
+
+ const student = new Student('Moshe', 'Ben' , []);
